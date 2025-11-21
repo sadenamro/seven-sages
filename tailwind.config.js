b8a6c320a83cd868d348ba225e81f5fad28abf5c
saden <sadenamr999@gmail.com>
2025-11-21 23:47:44 +0200

    feat: added missing tailwind config files to root

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// Located in the ROOT directory
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // This line scans all your JavaScript/React files for Tailwind classes
+    "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
