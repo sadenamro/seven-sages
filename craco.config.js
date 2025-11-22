329dacb143b2aef4332c338407fdd48fc12b8d89
saden <sadenamr999@gmail.com>
2025-11-22 04:22:09 +0200

    Fix Tailwind CSS configuration for Vercel build

--- craco.config.js
+++ craco.config.js
@@ -1,20 +1,11 @@
 // craco.config.js
 module.exports = {
-  // EXISTING style configuration remains here
-  style: { 
+  style: {
     postcss: {
       plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
       ],
-    },
-  },
-  // ADD THIS NEW LINE:
-  webpack: {
-    configure: (webpackConfig) => {
-      // This tells CRA to copy public folder files like manifest.json
-      webpackConfig.resolve.extensions.push('.js', '.jsx', '.ts', '.tsx');
-      return webpackConfig;
     },
   },
 };
