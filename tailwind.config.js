2c6a84dbf6cf727f2b61cb70833b3ce575b6f85b
saden <sadenamr999@gmail.com>
2025-11-22 04:30:51 +0200

    Fix Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-// Located in the ROOT directory
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // This line scans all your JavaScript/React files for Tailwind classes
-    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
